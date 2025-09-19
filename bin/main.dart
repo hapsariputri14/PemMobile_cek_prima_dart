e3a42795b6c8bc7c4d567978970293cdb89ed000
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-09-19 12:22:03 +0700

    Initial commit: Setup Dart project structure

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:pemmobile_cek_prima_dart/pemmobile_cek_prima_dart.dart' as pemmobile_cek_prima_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pemmobile_cek_prima_dart.calculate()}!');
+}
---
+++ test/pemmobile_cek_prima_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:pemmobile_cek_prima_dart/pemmobile_cek_prima_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

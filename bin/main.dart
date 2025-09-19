c338e52ef00ae4731c7eda58a0874fff9f10d866
Nadya Hapsari Putri <nadya.hp14@gmail.com>
2025-09-19 12:50:44 +0700

    feat: add input validation with if-else

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,24 @@
-import 'package:pemmobile_cek_prima_dart/pemmobile_cek_prima_dart.dart' as pemmobile_cek_prima_dart;
+void main() {
+  print('=== PROGRAM CEK BILANGAN PRIMA ===');
+  print('Tugas Mandiri Pemrograman Mobile');
+  print('Implementasi Percabangan dan Perulangan\n');
 
-void main(List<String> arguments) {
-  print('Hello world: ${pemmobile_cek_prima_dart.calculate()}!');
+  // Simulasi input (misalnya angka 17)
+  int number = getValidInput(17);
+  print('Input berhasil divalidasi: $number');
+  print('Program akan dikembangkan lebih lanjut...');
 }
+
+/// Fungsi untuk validasi input
+int getValidInput(int input) {
+  if (input <= 1) {
+    throw Exception('Error: Angka harus lebih besar dari 1!');
+  } else if (input > 1000000) {
+    throw Exception('Error: Angka terlalu besar! Maksimal 1.000.000');
+  } else {
+    print('Input valid!');
+    return input;
+  }
+}
+
+
